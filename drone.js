ede0c83e4f2a3541da936ab6e73b543b8675d1ef
Patti Shin <patricia.shin@willowtreeapps.com>
2014-05-29 15:02:38 -0400

    committing the weirdness

--- drone.js
+++ drone.js
@@ -1,14 +1,39 @@
 
 var arDrone = require('ar-drone');
-var client  = arDrone.createClient();
-var pngStream = client.getPngStream();
+var starfox = require('starfox');
 
-pngStream.on('data', console.log);
 
-/*client.on('navdata', function(data) {
+var http = require('http'),
+    fs = require('fs'),
+        path = require('path');
+
+    var server = http.createServer(function(request, response) {
+     var testFile = fs.createReadStream(path.join(__dirname, 'index.html'));
+                     testFile.pipe(response);
+    });
+            
+starfox.on('connection', function(player) {
+    player.on('input', function(gamepadState) {
+        console.log(gamepadState);
+    });
+    player.on('gamepadsChanged', function(gamepads) {
+        console.log(gamepads);
+    });
+});
+
+starfox.mount(server);
+server.listen(3000);
+
+//var client  = arDrone.createClient();
+//var pngStream = client.getPngStream();
+/*require('ar-drone-png-stream')(client, { port: 8000 });
+
+//pngStream.on('data', console.log);
+
+client.on('navdata', function(data) {
     console.log(data.demo.drone.camera);
     console.log(data.demo.detection.camera);
-});*/
+});
 
 //console.log(client);
 client.takeoff();
@@ -20,11 +45,34 @@ client.takeoff();
 // 3) move down to beach ball level
 
 client
-  .after(3000, function() {
-    this.front(0.1);
-    this.down(0.1);
+  .after(5000, function() {
+    //this.front(0.05);
+    this.down(0.05);
   })
   .after(5000, function() {
     this.stop();
     this.land();
   });
+*/
+
+//var autonomy = require('ardrone-autonomy');
+//var mission  = autonomy.createMission();
+
+/*mission.takeoff()
+       .zero()       // Sets the current state as the reference
+       .altitude(1)  // Climb to altitude = 1 meter
+       .forward(1)   
+       .right(1.7)     
+       .backward(1) 
+       .left(1.7);
+
+mission.run(function (err, result) {
+    if (err) {
+        console.trace("Oops, something bad happened: %s", err.message);
+        mission.client().stop();
+        mission.client().land();
+    } else {
+        console.log("Mission success!");
+        process.exit(0);
+    }
+});*/
