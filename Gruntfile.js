c3432a184f669a219a68a20ae4222f708e1e6a96
Patti Shin <patricia.shin@willowtreeapps.com>
2014-05-29 12:06:57 -0400

    getting tinkerdrone (backtickcoffee) to work

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,52 +2,18 @@
 module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: ['src/**/*.js'],
-                dest: 'dist/drone-main.js'
-            }
-        },
-        browserify: {
-            main: {
-                src: ['dist/drone-main.js'],
-                dest: 'dist/drone-bundle.js'
-            }
-        },
-        jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-            options: {
-                globals: {
-                    jQuery: true,
-                    console: true,
-                    module: true,
-                    document: true
-                }
-            }
-        },
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint'],
-            js: {
-                files: [ 'src/**/*.js' ],
-                tasks: [ 'browserify' ]
+        ffmpeg: {
+            video: {
+                files: [{
+                    expand: true,
+                    cwd: 'video_source',
+                    src: ['*.avi', '*.flv'],
+                    dest: 'dist',
+                    ext: '.mp4'
+                }]
             }
         }
     });
-    
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-browserify');
-
-    grunt.registerTask('default', [
-        'jshint',
-        'concat', 
-        'browserify'
-    ]);
+    grunt.loadNpmTasks('grunt-ffmpeg');
+    grunt.registerTask('default', ['ffmpeg']);
 };
