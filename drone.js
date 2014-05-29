1e377d09d3bd089afd1505284927d64de710c52b
Christine Bryant-Ryback <cbryantryback@gmail.com>
2014-05-29 15:34:17 -0400

    no work good :C

--- drone.js
+++ drone.js
@@ -1,6 +1,7 @@
 
 var arDrone = require('ar-drone');
 var starfox = require('starfox');
+var client  = arDrone.createClient();
 
 
 var http = require('http'),
@@ -15,56 +16,24 @@ var http = require('http'),
 starfox.on('connection', function(player) {
     player.on('input', function(gamepadState) {
         console.log(gamepadState);
-    });
-    player.on('gamepadsChanged', function(gamepads) {
-        console.log(gamepads);
+        //client.takeoff();
     });
 });
 
 starfox.mount(server);
 server.listen(3000);
 
-//var client  = arDrone.createClient();
-//var pngStream = client.getPngStream();
-/*require('ar-drone-png-stream')(client, { port: 8000 });
 
-//pngStream.on('data', console.log);
+var autonomy = require('ardrone-autonomy');
+var mission  = autonomy.createMission();
 
-client.on('navdata', function(data) {
-    console.log(data.demo.drone.camera);
-    console.log(data.demo.detection.camera);
-});
-
-//console.log(client);
-client.takeoff();
-
-
-
-// 1) launch
-// 2) move forward
-// 3) move down to beach ball level
-
-client
-  .after(5000, function() {
-    //this.front(0.05);
-    this.down(0.05);
-  })
-  .after(5000, function() {
-    this.stop();
-    this.land();
-  });
-*/
-
-//var autonomy = require('ardrone-autonomy');
-//var mission  = autonomy.createMission();
-
-/*mission.takeoff()
+mission.takeoff()
        .zero()       // Sets the current state as the reference
-       .altitude(1)  // Climb to altitude = 1 meter
-       .forward(1)   
-       .right(1.7)     
-       .backward(1) 
-       .left(1.7);
+       .altitude(1);  // Climb to altitude = 1 meter
+       // .forward(1)   
+       // .right(1.7)     
+       // .backward(1) 
+       // .left(1.7);
 
 mission.run(function (err, result) {
     if (err) {
@@ -75,4 +44,19 @@ mission.run(function (err, result) {
         console.log("Mission success!");
         process.exit(0);
     }
-});*/
+});
+
+// 1) launch
+// 2) move forward
+// 3) move down to beach ball level
+
+/*client
+  .after(5000, function() {
+    //this.front(0.05);
+    this.down(0.05);
+  })
+  .after(5000, function() {
+    this.stop();
+    this.land();
+  });
+*/
